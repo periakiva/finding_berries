a25847d9ed1d87092ad347b375d164a596331ca4
periakiva <periha@gmail.com>
2020-05-29 14:07:43 -0400

    directory cleanup

--- training/cranberry_segmentation/train.py
+++ training/cranberry_segmentation/train.py
@@ -1,7 +1,3 @@
-"""
-Main training file. Config file should be in the same directory.
-"""
-
 import sys
 import os
 current_path = os.getcwd().split("/")
@@ -35,12 +31,12 @@ from skimage.segmentation import find_boundaries
 from skimage import morphology
 import warnings
 import yaml
-import floodfill.losses as losses
+import losses
 warnings.filterwarnings('ignore')
 
 class Trainer(object):
     def __init__(self,model,train_loader,val_loader,epochs,optimizer,scheduler,
-                    criterion,losses_to_use,test_loader = None, test_with_full_supervision = 0,loss_weights={}},class_weights={}):
+                    criterion,losses_to_use,test_loader = None, test_with_full_supervision = 0,loss_weights=[1.0,1.0,1.0],class_weights={}):
 
         """
         :param model: PyTorch module, model to be trained
@@ -74,122 +70,6 @@ class Trainer(object):
         if test_loader is not None:
             self.test_loader = test_loader
             self.test_with_full_supervision = test_with_full_supervision
-        else:
-            self.test_with_full_supervision = test_with_full_supervision
-
-    def train(self,epoch,cometml_experiemnt):
-        total_loss = 0 
-        # total_loss_dict = {"inst_loss""cvx_loss""circ_loss""closs"}
-        self.model.train()
-        losses_dict = {'seg_loss':0.0,'inst_loss':0.0,'cvx_loss':0.0,'circ_loss':0.0,'closs':0.0}
-        for batch_index,batch in enumerate(self.train_loader):
-            imgs,masks,count = batch
-            imgs = imgs.to(device)
-            masks = masks.to(device).squeeze(1)
-            self.optimizer.zero_grad()
-
-            loss, loss_dict = losses.count_segment_loss(model,batch,self.losses_to_use,self.loss_weights,self.class_weights)
-            loss.backward()
-            self.optimizer.step()
-            total_loss +=loss.item()
-            # for key in loss_dict.keys():
-            #     losses_dict[key] +=loss_dict[key].item()
-
-        cometml_experiemnt.log_metric("Training Average Loss",total_loss/self.train_loader.__len__(),epoch=epoch+1)
-        for key in losses_dict.keys():
-            cometml_experiemnt.log_metric("Training " + key +" Loss",losses_dict[key]/self.train_loader.__len__(),epoch=epoch+1)
-            
-        print("Training Epoch {0:2d} average loss: {1:1.2f}".format(epoch+1, total_loss/self.train_loader.__len__()))
-
-        return total_loss/self.train_loader.__len__()
-            
-    def validate(self,epoch,cometml_experiemnt):
-        self.model.eval()
-        print("validating")
-        total_loss = 0
-        preds,targets,counts,estimated_counts,detection_counts = [],[],[],[], []
-        loss_type = "_".join(self.losses_to_use)
-        loss_weights_str = "_".join([str(x)+str(y) for x,y in self.loss_weights.items()])
-        if self.test_with_full_supervision == 1:
-            loader = self.test_loader
-        else:
-            loader = self.val_loader
-        with torch.no_grad():
-            for batch_index,batch in enumerate(loader):
-                imgs,masks,count = batch
-
-                imgs = imgs.to(device)
-                masks = masks.to(device).squeeze(1)
-                count = count.to(device)
-                output, count_estimation = self.model.forward(imgs)
-                
-                loss = self.criterion(output,masks)
-                pred = output.max(1)[1].squeeze_(1).squeeze_(0).cpu().numpy()
-                
-                blobs = pred==1
-                labels, nlabels = ndimage.label(blobs)
-                count_by_detection = 0
-                for label in range(1,nlabels):
-                    inds = np.argwhere(labels==label)
-                    area = inds.shape[0]
-                    x = inds[:,0]
-                    y = inds[:,1]
-                    if area < 25:
-                        labels[x,y] = 0
-                    if area > 25:
-                        count_by_detection = count_by_detection + 1
-
-
-
-                if self.visualizer_indicator:
-                    if (epoch+1)%1 == 0:
-                        figure = self.visualizer(pred,imgs,masks,epoch,loss_type,count_estimation.item(),count.item())
-                        cometml_experiemnt.log_figure(figure_name=f"epoch: {epoch}, current loss: {loss}",figure=figure)
-                        # model.predict(batch)
-                        if self.save_figures:
-                            # print("saving image")
-                            figure_save_path = f"/home/native/projects/cranberry_counting/visualization_outputs/points/{loss_type}_{loss_weights_str}/"
-                            utils.create_dir_if_doesnt_exist(figure_save_path)
-                            figure.savefig(f"{figure_save_path}/epoch_{epoch}_loss_{loss}_estimatedcount_{count_estimation.item()}_gt_count_{count.item()}.png",dpi=300)
-                        figure.clear()
-                        plt.cla()
-                        plt.clf()
-                        plt.close('all')
-                        plt.close(figure)
-                        gc.collect()
-                masks = masks.squeeze_(0).cpu().numpy()
-                preds.append(pred)
-                targets.append(masks)
-                counts.append(count.item())
-                estimated_counts.append(count_estimation.item())
-                detection_counts.append(count_by_detection)
-                total_loss+=loss.item()
-        # val_mae_lcfcn = eval_utils.val_mae(estimated_counts,counts)
-        count_mae = eval_utils.mae(estimated_counts,counts)
-        count_rmse = eval_utils.rmse(estimated_counts,counts)
-        count_mape = eval_utils.mape(estimated_counts,counts)
-
-        detection_count_mae = eval_utils.mae(detection_counts,counts)
-        detection_count_rmse = eval_utils.rmse(detection_counts,counts)
-        detection_count_mape = eval_utils.mape(detection_counts,counts)
-        count_metrics = {"regression mae":count_mae,"regression rmse":count_rmse,"regression mape":
-                        count_mape,"detection mae":detection_count_mae,"detection rmse":detection_count_rmse,
-                        "detection mape":detection_count_mape}
-        # print(type(count_metrics[0]))
-        _,_,mean_iou,_ = eval_utils.calc_mAP(preds,targets)
-        print("Validation mIoU value: {0:1.5f}".format(mean_iou))
-        print(f"Validation Count Regression Mean Average Error: {count_mae}\nRegression Root Mean Squared Error: {count_rmse}\nRegression Mean Absolute Percent Error: {count_mape}\nDetection MAE: {detection_count_mae}\nDetection RMSE: {detection_count_rmse}\n Detection MAPE: {detection_count_mape}")
-        print("Validation Epoch {0:2d} average loss: {1:1.2f}".format(epoch+1, total_loss/self.val_loader.__len__()))
-        cometml_experiemnt.log_metric("Validation mIoU",mean_iou,epoch=epoch+1)
-        cometml_experiemnt.log_metric("Validation Mean Average Error",count_mae,epoch=epoch+1)
-        cometml_experiemnt.log_metric("Validation Root Mean Squared Error",count_rmse,epoch=epoch+1)
-        cometml_experiemnt.log_metric("Validation Mean Absolute Percent Error",count_mape,epoch=epoch+1)
-        cometml_experiemnt.log_metric("Validation Detection Mean Average Error",detection_count_mae,epoch=epoch+1)
-        cometml_experiemnt.log_metric("Validation Detection Root Mean Squared Error",detection_count_rmse,epoch=epoch+1)
-        cometml_experiemnt.log_metric("Validation Detection Mean Absolute Percent Error",detection_count_mape,epoch=epoch+1)
-        cometml_experiemnt.log_metric("Validation Average Loss",total_loss/self.val_loader.__len__(),epoch=epoch+1)
-
-        return total_loss/self.val_loader.__len__(), mean_iou,count_metrics
 
     def visualizer(self,pred,imgs,masks,epoch,loss_type,estimated_count,gt_count):
 
@@ -244,8 +124,113 @@ class Trainer(object):
         
         return fig
 
-    def forward(self,cometml_experiment):
+    def train(self,epoch,cometml_experiemnt):
+        total_loss = 0 
+        # total_loss_dict = {"inst_loss""cvx_loss""circ_loss""closs"}
+        self.model.train()
+        losses_dict = {'seg_loss':0.0,'inst_loss':0.0,'cvx_loss':0.0,'circ_loss':0.0,'closs':0.0}
+        for batch_index,batch in enumerate(self.train_loader):
 
+            imgs,masks,count = batch
+            imgs = imgs.to(device)
+            masks = masks.to(device).squeeze(1)
+            self.optimizer.zero_grad()
+
+            loss, loss_dict = losses.count_segment_loss(model,batch,self.losses_to_use,self.loss_weights,self.class_weights)
+            loss.backward()
+            self.optimizer.step()
+            total_loss +=loss.item()
+            for key in loss_dict.keys():
+                losses_dict[key] +=loss_dict[key].item()
+
+        cometml_experiemnt.log_metric("Training Average Loss",total_loss/self.train_loader.__len__(),epoch=epoch+1)
+        for key in losses_dict.keys():
+            cometml_experiemnt.log_metric("Training " + key +" Loss",losses_dict[key]/self.train_loader.__len__(),epoch=epoch+1)
+            
+        print("Training Epoch {0:2d} average loss: {1:1.2f}".format(epoch+1, total_loss/self.train_loader.__len__()))
+
+        return total_loss/self.train_loader.__len__()
+            
+    def validate(self,epoch,cometml_experiemnt):
+        self.model.eval()
+        print("validating")
+        total_loss = 0
+        preds,targets,counts,estimated_counts,detection_counts = [],[],[],[], []
+        loss_type = "_".join(self.losses_to_use)
+        loss_weights_str = "_".join([str(x)+str(y) for x,y in self.loss_weights.items()])
+        if self.test_with_full_supervision == 1:
+            loader = self.test_loader
+        else:
+            loader = self.val_loader
+        with torch.no_grad():
+            for batch_index,batch in enumerate(loader):
+                imgs,masks,count = batch
+
+                imgs = imgs.to(device)
+                masks = masks.to(device).squeeze(1)
+                count = count.to(device)
+
+                output, count_estimation = self.model.forward(imgs)
+                
+                loss = self.criterion(output,masks)
+                pred = output.max(1)[1].squeeze_(1).squeeze_(0).cpu().numpy()
+                
+                blobs = pred==1
+                labels, nlabels = ndimage.label(blobs)
+                
+                count_by_detection = utils.filterer(labels,nlabels)
+
+                if self.visualizer_indicator:
+                    if (epoch+1)%1 == 0:
+                        figure = self.visualizer(pred,imgs,masks,epoch,loss_type,count_estimation.item(),count.item())
+                        cometml_experiemnt.log_figure(figure_name=f"epoch: {epoch}, current loss: {loss}",figure=figure)
+                        # model.predict(batch)
+                        if self.save_figures:
+                            # print("saving image")
+                            figure_save_path = f"/home/native/projects/cranberry_counting/visualization_outputs/points/{loss_type}_{loss_weights_str}/"
+                            utils.create_dir_if_doesnt_exist(figure_save_path)
+                            figure.savefig(f"{figure_save_path}/epoch_{epoch}_loss_{loss}_estimatedcount_{count_estimation.item()}_gt_count_{count.item()}.png",dpi=300)
+                        figure.clear()
+                        plt.cla()
+                        plt.clf()
+                        plt.close('all')
+                        plt.close(figure)
+                        gc.collect()
+                masks = masks.squeeze_(0).cpu().numpy()
+                preds.append(pred)
+                targets.append(masks)
+                counts.append(count.item())
+                estimated_counts.append(count_estimation.item())
+                detection_counts.append(count_by_detection)
+                total_loss+=loss.item()
+        # val_mae_lcfcn = eval_utils.val_mae(estimated_counts,counts)
+        count_mae = eval_utils.mae(estimated_counts,counts)
+        count_rmse = eval_utils.rmse(estimated_counts,counts)
+        count_mape = eval_utils.mape(estimated_counts,counts)
+
+        detection_count_mae = eval_utils.mae(detection_counts,counts)
+        detection_count_rmse = eval_utils.rmse(detection_counts,counts)
+        detection_count_mape = eval_utils.mape(detection_counts,counts)
+        count_metrics = {"regression mae":count_mae,"regression rmse":count_rmse,"regression mape":
+                        count_mape,"detection mae":detection_count_mae,"detection rmse":detection_count_rmse,
+                        "detection mape":detection_count_mape}
+        # print(type(count_metrics[0]))
+        _,_,mean_iou,_ = eval_utils.calc_mAP(preds,targets)
+        print("Validation mIoU value: {0:1.5f}".format(mean_iou))
+        print(f"Validation Count Regression Mean Average Error: {count_mae}\nRegression Root Mean Squared Error: {count_rmse}\nRegression Mean Absolute Percent Error: {count_mape}\nDetection MAE: {detection_count_mae}\nDetection RMSE: {detection_count_rmse}\n Detection MAPE: {detection_count_mape}")
+        print("Validation Epoch {0:2d} average loss: {1:1.2f}".format(epoch+1, total_loss/self.val_loader.__len__()))
+        cometml_experiemnt.log_metric("Validation mIoU",mean_iou,epoch=epoch+1)
+        cometml_experiemnt.log_metric("Validation Mean Average Error",count_mae,epoch=epoch+1)
+        cometml_experiemnt.log_metric("Validation Root Mean Squared Error",count_rmse,epoch=epoch+1)
+        cometml_experiemnt.log_metric("Validation Mean Absolute Percent Error",count_mape,epoch=epoch+1)
+        cometml_experiemnt.log_metric("Validation Detection Mean Average Error",detection_count_mae,epoch=epoch+1)
+        cometml_experiemnt.log_metric("Validation Detection Root Mean Squared Error",detection_count_rmse,epoch=epoch+1)
+        cometml_experiemnt.log_metric("Validation Detection Mean Absolute Percent Error",detection_count_mape,epoch=epoch+1)
+        cometml_experiemnt.log_metric("Validation Average Loss",total_loss/self.val_loader.__len__(),epoch=epoch+1)
+
+        return total_loss/self.val_loader.__len__(), mean_iou,count_metrics
+    
+    def forward(self,cometml_experiment):
         train_losses = []
         val_losses = []
         mean_ious_val,mean_ious_val_list,count_metrics_list = [], [], []
@@ -259,7 +244,6 @@ class Trainer(object):
         counting_type = self.losses_to_use[-1]
         model_save_dir = config['data']['model_save_dir']+f"{current_path[-1]}/{cometml_experiment.project_name}_{empty_string.join(self.losses_to_use)}_{loss_weights_str}_{datetime.datetime.today().strftime('%Y-%m-%d-%H:%M')}/"
         utils.create_dir_if_doesnt_exist(model_save_dir)
-        
         for epoch in range(0,self.epochs):
             with cometml_experiment.train():
                 train_loss = self.train(epoch,cometml_experiment)
@@ -286,7 +270,6 @@ class Trainer(object):
                     best_miou_to_mae_ratio = miou_to_mae_ratio
                         
                     with open(model_save_dir+"config.yaml",'w') as file:
-                        print(mean_ious_val)
                         config['count_metrics'] = count_metrics_list
                         config['mean_ious_val'] = mean_ious_val_list
                         config['val_losses'] = val_losses
@@ -305,8 +288,6 @@ if __name__== "__main__":
     
     config_path = utils.dictionary_contents(os.getcwd()+"/",types=["*.yaml"])[0]
     config = utils.config_parser(config_path,experiment_type="training")
-    # with open(config_path) as file:
-    #     config = yaml.load(file,Loader=yaml.FullLoader)
     config['start_time'] = datetime.datetime.today().strftime('%Y-%m-%d-%H:%M:%S')
     
     torch.set_default_dtype(torch.float32)
@@ -318,8 +299,7 @@ if __name__== "__main__":
                                                                                                                 num_workers=config['training']['num_workers'],type=config['data']['type'],
                                                                                                                 train_val_test_split=config['training']['train_val_test_split']
                                                                                                                 )
-    # fs_test_loader = cranberry_dataset.build_single_loader(config['data']['test_dir'],batch_size=1,num_workers=1,test=True)
-    fs_test_loader = None
+    fs_test_loader = cranberry_dataset.build_single_loader(config['data']['test_dir'],batch_size=1,num_workers=1,test=True)
     with peter('Building Network'):
         model = unet_refined.UNetRefined(n_channels=3,n_classes=2)
         # model = unet_regres.Unet(in_channels=3,classes=2,decoder_channels= (512,256,128),encoder_depth=3)
@@ -328,7 +308,6 @@ if __name__== "__main__":
     # model = nn.DataParallel(model)
     model.to(device)
     model.cuda()
-    
 
     class_weights = torch.Tensor((1,1)).float()
     class_weights = class_weights.to(device)
@@ -341,32 +320,28 @@ if __name__== "__main__":
     start_epoch = 0
     lowest_mahd = np.infty
 
+    #TODO: Add resume option to Trainer using below code
     if config['training']['resume'] != False:
         with peter('Loading checkpoints'):
             if os.path.isfile(config['training']['resume']):
                 # model = torch.load(config['training']['resume'])
                 checkpoint = torch.load(config['training']['resume'])
-                # print(checkpoint)
                 start_epoch = checkpoint['epoch']
                 model.load_state_dict(checkpoint['model'])
                 optimizer.load_state_dict(checkpoint['optimizer'])
                 scheduler.load_state_dict(checkpoint['scheduler'])
                 print(f"loaded model from {config['training']['resume']}")
-                # print("Loaded checkpoint {}, now at epoch: {}".format(config['training']['resume'],checkpoint['epoch']))        
             else:
                 print("no checkpoint found at {}".format(config['training']['resume']))
                 exit()
-
     print(f"using losses: {config['training']['losses_to_use']}")
-    # running_average = utils.RunningAverage(len(train_dataloader))
+
     trainer = Trainer(model,train_dataloader,validation_dataloader,config['training']['epochs'],
                         optimizer,scheduler,loss_segmentation,losses_to_use=config['training']['losses_to_use'],
                         test_loader=fs_test_loader,
                         test_with_full_supervision=config['training']['test_with_full_supervision'],
                         loss_weights=config['training']['loss_weights'],class_weights = config['training']['class_weights'])
-    
     train_losses, val_losses, mean_ious_val = trainer.forward(experiment)
-
 
 
             
